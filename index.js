8beeae398f81852f7b5074ec6bcc7d35e5f3c801
dingcong123456 <1049141971@qq.com>
2017-03-03 21:19:33 +0800

    2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express=require('express');
+var indexRouter=require('./routes/index');
+var usersRouter=require('./routes/users');
 var app=new express();
-app.get('/',function(req,res){
-    res.send('helloword');
-}).listen(3000);
+app.use('/',indexRouter);
+app.use('/users',usersRouter);
+app.listen(3000);
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+var router=require('express').Router();
+router.get('/',function (req,res) {
+    res.send('hello,express');
+});
+module.exports=router;
