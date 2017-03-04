2e3d49add6a31710fc397d296aee2ec309715839
Rain <1308122985@qq.com>
2017-03-04 20:02:01 +0800

    33

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-var router=require('express').Router();
-router.get('/',function (req,res) {
-    res.send('hello,express');
-});
-module.exports=router;
+module.exports = function (app) {
+    app.get('/', function (req, res) {
+        res.redirect('/posts');
+    });
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+};
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
-var express=require('express');
-var router=express.Router();
-router.get('/:name',function(req,res){
-    res.send('hello, '+req.params.name);
+var express = require('express');
+var router = express.Router();
+router.get('/:name', function (req, res) {
+    res.render('users', {
+        name: req.params.name
+    });
 });
-module.exports=router;
+module.exports = router;
